a2e29dcb0e85c5b9a155c4e1c0b5acf6910f8070
Artur Signell <artur@vaadin.com>
2019-09-02 16:01:35 +0300

    Data versions of SVGs

--- makedist.sh
+++ makedist.sh
@@ -4,6 +4,8 @@ cp src/a-avataaar.js dist/
 for svg in `find src/assets -type f -name "*.svg"`
 do
 	find="$svg"
-	replace=`node svg-to-dataurl.js $svg`
+	dataSvg=`echo $svg|sed "s/assets/assets-dataurl/g"`
+#	replace=`node svg-to-dataurl.js $svg`
+	replace=`cat $dataSvg`
 	gsed -i "s@$find@$replace@g" dist/a-avataaar.js
 done
