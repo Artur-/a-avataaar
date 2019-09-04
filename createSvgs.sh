196fcad79cf573487d8ae55a812cad52cfe6732b
Artur Signell <artur@vaadin.com>
2019-09-04 10:43:20 +0300

    Delete old scripts and move remaining to separate folder

--- createSvgs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for orig in `find src/assets-original -name "*.svg"`
-do 
-  asset=`echo $orig|gsed "s/-original//"`
-  assetdata=`echo $orig|gsed "s/-original/-dataurl/"`
-  node_modules/svgo/bin/svgo -i $orig -o $asset
-  node svg-to-dataurl.js $asset > $assetdata
-done
--- svg-to-dataurl.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-const svgToDataURL = require('svg-to-dataurl')
-const svgFile = process.argv[2];
-const svg = fs.readFileSync(svgFile);
-const dataUrl = svgToDataURL(svg)
-console.log(dataUrl)
